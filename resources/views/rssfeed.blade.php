37ac37b2a2a46d7f735f04cbdf8ee05cdc12c779
Amna Noor <amnanooraslam@gmail.com>
2024-10-09 11:12:55 +0500

    slide error fixed

--- resources/views/rssfeed.blade.php
+++ resources/views/rssfeed.blade.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>News Feed</title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    {{-- <style>
-       
-    </style> --}}
 </head>
 <body>
    
